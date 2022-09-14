f26ba5311f0711764bf91a63649ab176a36a544d
khushbu.satasiya <nisargsatasiya1@gmail.com>
2022-09-14 12:45:34 +0530

    tailwind project beauty product page

---
+++ tailwind projects/RWD/Photographer page/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'alpha': ["Alfa Slab One", "cursive"],
+        'monster': ["Montserrat", "sans - serif"]
+      },
+    },
+  },
+  plugins: [],
+}
