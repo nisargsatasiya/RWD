189dd223ff42004129ef578977d9c9263df6c978
khushbu.satasiya <nisargsatasiya1@gmail.com>
2022-09-15 19:24:11 +0530

    Ahhoy page

---
+++ tailwind projects/RWD/Ahoyy page/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Arial': ["Arial", "Helvetica", "sans-serif"]
+      },
+    },
+  },
+  plugins: [],
+}
